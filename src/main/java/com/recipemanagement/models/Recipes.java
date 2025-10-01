877c1d5418d33dec6d17c524602d5d557f587c76
arcticcloudfox <27johnson279@gmail.com>
2025-09-30 21:11:17 -0500

    added database variables to Recipes and Users

--- src/main/java/com/recipemanagement/models/Recipes.java
+++ src/main/java/com/recipemanagement/models/Recipes.java
@@ -1,4 +1,36 @@
 package com.recipemanagement.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Setter;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Recipes {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotBlank(message = "Recipe name is required!")
+    private String name;
+
+    private String recipeType;
+
+    @NotEmpty(message = "Ingredients are required!")
+    private String ingredients;
+
+    @NotEmpty(message = "Instructions are required!")
+    private String instructions;
+
+    private LocalDateTime recipeWasAdded;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
 }
--- src/main/java/com/recipemanagement/models/Users.java
+++ src/main/java/com/recipemanagement/models/Users.java
@@ -1,4 +1,28 @@
 package com.recipemanagement.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+@Getter
+@Setter
 public class Users {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Recipes> recipes;
+
 }
