72d0e29c0caae791cd6623e69cebdbb2d9a3b1f4
arcticcloudfox <27johnson279@gmail.com>
2026-01-04 20:33:20 -0600

    Added main class to run application

--- src/main/java/com/recipemanagement/RecipeManagementApplication.java
+++ src/main/java/com/recipemanagement/RecipeManagementApplication.java
@@ -1,4 +1,11 @@
 package com.recipemanagement;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RecipeManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeManagementApplication.class, args);
+    }
 }
